0db4a480a2ef81a992e8370fbd7490f623c4117a
Peter Yuan <peter.bond40@gmail.com>
2025-01-24 11:31:18 -0600

    update profile page layout

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,73 +1,91 @@
 <style>
-    .box-father{
-        display:flex;
-        justify-content:center;
-    }
-    .box-child{   
-        width:400px;
-        border:3px solid black;
-        background-color:#fff7d1;
-        border-radius:10px;
-        padding:15px;
-        
+    .box-father {
+        display: flex;
+        justify-content: center;
     }
 
-    .h-title{
-        text-align:center;
+    .box-child {
+        width: 400px;
+        border: 3px solid black;
+        background-color: #fff7d1;
+        border-radius: 10px;
+        padding: 15px;
+
     }
-    .text-father{
-        display:flex;
-        justify-content:center;
+
+    .h-title {
+        text-align: center;
+    }
+
+    .text-father {
+        display: flex;
+        justify-content: center;
     }
 </style>
 
 
 <div class="box-father">
     <div class="box-child">
-    <div class="h-title">
+        <div class="h-title">
             <h1>Profile</h1>
         </div>
         <div class="text-father">
-            <div class="text-child"> 
-    <h3>username: <?php echo $username ?></h3>
+            <div class="text-child">
 
-    <h3>firstname: <?php echo $firstname ?></h3>
-    <h3>lastname: <?php echo $lastname ?></h3>
-    <h3>email: <?php echo $email ?></h3>
+                <?php echo form_open("user/update") ?>
+                <fieldset>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input class="form-control" disabled type="text" id="username" name='username'
+                            value=<?php echo $username ?>>
 
-    <!-- update can apply only to firstname and last name, and update time applied. -->
+                    </div>
+                    <div class="form-group">
+                        <label for="firstname">Firstname</label>
+                        <input class="form-control" type="text" id="firstname" name='firstname'
+                            value=<?php echo $firstname ?>>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Lastname</label>
+                        <input class="form-control" type="text" id="lastname" name='lastname'
+                            value=<?php echo $lastname ?>>
+
+                    </div>
+                    <input type="hidden" name='id'
+                        value=<?php echo $id ?>>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input class="form-control" disabled type="text" id="email" name='email'
+                            value=<?php echo $email ?>>
+
+                    </div>
+
+                    <input class="btn btn-primary mt-2" type="submit" value="update">
+
+                </fieldset>
+                </form>
 
 
-    <?php echo form_open("user/update") ?>
-    <fieldset>
-        <div class="form-group">
-        <label for="firstname">Firstname</label>
-        <input class="form-control" type="text" id="firstname" name='firstname' value=<?php echo $firstname ?>>
-        
-        </div>
-        <div class="form-group">
-        <label for="lastname">Lastname</label>
-        <input class="form-control" type="text" id="lastname" name='lastname' value=<?php echo $lastname ?>>
-        
-        </div>
-        <input type="hidden" name='id' value=<?php echo $id ?>>
-        <input class="btn btn-primary mt-2" type="submit" value="update">
-    </fieldset>
-    </form>
 
-    
-    
+                <!-- update can apply only to firstname and last name, and update time applied. -->
 
-    <?php echo form_open("user/logout") ?>
-        <input class="btn btn-success mt-2" type="submit" value="logout">
-    </form>
 
-    
-    
-    <!-- <?php echo form_open("user/deactivate") ?>
-        <input class="btn btn-danger mt-2" type="submit" value="Close account">
-    </form> -->
-    </div>
+
+
+
+
+
+                <?php echo form_open("user/logout") ?>
+                <input class="btn btn-success mt-2" type="submit" value="logout">
+                </form>
+
+
+
+                <!-- <?php echo form_open("user/deactivate") ?>
+                <input class="btn btn-danger mt-2" type="submit" value="Close account">
+                </form> -->
+            </div>
         </div>
     </div>
 </div>
